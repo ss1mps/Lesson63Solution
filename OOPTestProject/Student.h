19a6ab3fc2cbd82c9da01f2aa53254b0716cea47
Tsimofey Terashkevich <t861727861727@gmail.com>
2026-06-19 18:52:53 +0300

    Do main05

--- OOPTestProject/Student.h
+++ OOPTestProject/Student.h
@@ -10,4 +10,63 @@ public:
 	double mark;
 	char gender;	
 	bool alive;
+
+	Student() {
+		cout << "Students default constructor\n";
+		name = "";
+		age = 0;
+		alive = false;
+		mark = 0;
+		gender = '\0';
+	}
+
+	Student(string nm, int ag) {
+		cout << "Students default constructor with arguments\n";
+		name = nm;
+		age = ag;
+		alive = false;
+		mark = 0;
+		gender = '\0';																					
+	}
+
+	Student(int ag, string nm) {
+		cout << "Students default constructor with arguments\n";
+		name = nm;
+		age = ag;
+		alive = false;
+		mark = 0;
+		gender = '\0';
+	}
+
+	Student(string nm, int ag, char gndr, double mrk, bool a) {
+		cout << "Students canonical constructor\n";
+		name = nm;
+		age = ag;
+		alive = a;
+		mark = mrk;
+		gender = gndr;
+	}
+
+	Student(const Student* student) {
+		cout << "Students copy-constructor\n";
+		name = student->name;
+		age = student->age;
+		alive = student->alive;
+		mark = student->mark;
+		gender = student->gender;
+	}
+
+	~Student() {
+		cout << "Students destructor\n";
+	}
+
+	string toString() {
+		string s = "";
+
+		s = "Students: " + name + ", age = " + to_string(age)
+			+ ", gender " + gender + ", mark = " + to_string(mark)
+			+ ", alive = " + (alive ? "yes" : "no");
+
+		return s;
+	}
 };
