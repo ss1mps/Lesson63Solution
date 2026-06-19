58cd0cd9af1062f50726062d72d2f49473522507
Tsimofey Terashkevich <t861727861727@gmail.com>
2026-06-19 19:15:45 +0300

    Do main07

--- OOPTestProject/main06.cpp
+++ OOPTestProject/main06.cpp
@@ -1,19 +1,19 @@
-#include "Student.h"
-
-int main() {
-
-	int* marks1 = new int[5]{ 5, 7, 8, 9, 10 };
-	int* marks2 = new int[3]{ 7, 9, 10 };
-
-
-	Student* st01 = new Student("Alex", 14, 'm', true, marks1, 5);
-	Student* st02 = new Student("Anna", 13, 'f', true, marks2, 3);
-
-	cout << st01->toString() << endl;
-	cout << st02->toString() << endl;
-
-	delete st01;
-	delete st02;
-
-	return 0;
-}
+//#include "Student.h"
+//
+//int main() {
+//
+//	int* marks1 = new int[5]{ 5, 7, 8, 9, 10 };
+//	int* marks2 = new int[3]{ 7, 9, 10 };
+//
+//
+//	Student* st01 = new Student("Alex", 14, 'm', true, marks1, 5);
+//	Student* st02 = new Student("Anna", 13, 'f', true, marks2, 3);
+//
+//	cout << st01->toString() << endl;
+//	cout << st02->toString() << endl;
+//
+//	delete st01;
+//	delete st02;
+//
+//	return 0;
+//}
